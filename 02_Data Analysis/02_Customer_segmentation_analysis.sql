52b4bdfa3f01582eb27c66dccf037e6cf2835526
Marcin Sprycha <133162601+marcinsprycha@users.noreply.github.com>
2026-04-13 22:50:24 +0200

    Delete 02_Data Analysis/02_Customer_segmentation_analysis.sql

--- 02_Data Analysis/02_Customer_segmentation_analysis.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    dc.EnglishEducation AS EducationLevel,
-    dc.MaritalStatus,
-    COUNT(DISTINCT dc.CustomerKey) AS NumberOfCustomers,
-    AVG(fis.SalesAmount) AS AverageOrderValue
-FROM DimCustomer dc
-JOIN FactInternetSales fis ON dc.CustomerKey = fis.CustomerKey
-GROUP BY dc.EnglishEducation, dc.MaritalStatus
-ORDER BY NumberOfCustomers DESC;
