1ff921f16906f5e6537c740c51c75490cf15830a
Marcin Sprycha <133162601+marcinsprycha@users.noreply.github.com>
2026-04-13 22:55:06 +0200

    Delete 01_Data_Cleaning/01_Sales_Analysis_by_Category.sql

--- 01_Data_Cleaning/01_Sales_Analysis_by_Category.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-    dp.EnglishProductCategoryName,
-    SUM(fis.SalesAmount) AS TotalSales,
-    COUNT(DISTINCT fis.ProductKey) AS NumberOfUniqueProductsSold
-FROM DimProduct dp
-JOIN FactInternetSales fis ON dp.ProductKey = fis.ProductKey
-GROUP BY dp.EnglishProductCategoryName
-ORDER BY TotalSales DESC;
